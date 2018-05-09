c5b199f43e351f79803ce60026f7db11d50fcf70
Thomas ZIMMER <thomas.zimmer@sigfox.com>
2018-05-09 17:18:01 +0200

    Web Server first

--- hello.go
+++ hello.go
@@ -3,16 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/zimwip/hello/stringutil"
+	"log"
+	"net/http"
+	"strings"
 )
 
-type PayloadCollection struct {
-	WindowsVersion string    `json:"version"`
-	Token          string    `json:"token"`
-	Payloads       []Payload `json:"data"`
-}
-
-type Payload struct {
-	// [redacted]
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()       // parse arguments, you have to call this by yourself
+	fmt.Println(r.Form) // print form information in server side
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello astaxie!") // send data to client side
 }
 
 func main() {
@@ -41,6 +47,12 @@ func main() {
 
 	result := stringutil.Reverse("Hello, world")
 	fmt.Printf(result)
+
+	http.HandleFunc("/", sayhelloName)       // set router
+	err := http.ListenAndServe(":9090", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
 
 func multiplyByTwo(in <-chan int, out chan<- int) {
